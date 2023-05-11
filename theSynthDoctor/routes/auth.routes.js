be3f762acc901f910dead5c6342bcfaee26632a0
Filippo Marinolli <f.marinolli@gmail.com>
2023-05-11 19:56:54 +0200

    SESSION COOKIES AND DB WORKING

--- theSynthDoctor/routes/auth.routes.js
+++ theSynthDoctor/routes/auth.routes.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const db = require("../db/index.js");
-const User = require('../models/User.model');
-const bcrypt = require("bcryptjs")
-const mongoose = require("mongoose")
-
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
@@ -12,101 +11,122 @@ const salt = bcrypt.genSaltSync(saltRounds);
 let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{7,}$/;
 
-
 router.get("/create", (req, res, next) => {
-    res.render("account-create");
-  });
-  
+  res.render("account-create");
+});
+
 router.post("/create", async (req, res, next) => {
   const newUser = req.body;
   console.log(newUser);
   if (!newUser.username || !newUser.password || !newUser.name) {
-      res.render("account-create", {
-        errorMessage:
-          "All fields are mandatory. Please provide your username, email and password.",
+    res.render("account-create", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+    return;
+  } else if (newUser.email != newUser.email2) {
+    res.render("account-create", {
+      errorMessage:
+        "The emails you provided are not the same, please check again",
+    });
+    return;
+  } else if (newUser.password != newUser.password2) {
+    res.render("account-create", {
+      errorMessage:
+        "The Passwords you provided are not the same, please check again",
+    });
+    return;
+  } else if (!passwordRegex.test(newUser.password)) {
+    res.render("account-create", {
+      errorMessage:
+        "The Passwords should contain at least 1 capital letter, 1 number and be minimum 7 characters long",
+    });
+    return;
+  } else if (!emailRegex.test(newUser.email)) {
+    res.render("account-create", {
+      errorMessage: "The Email you provided is invalid, please check again",
+    });
+    return;
+  }
+  try {
+    const pw = bcrypt.hashSync(newUser.password, salt);
+    newUser.password = pw;
+    await User.create({
+      name: newUser.name,
+      surname: newUser.surname,
+      username: newUser.username,
+      email: newUser.email,
+      password: newUser.password,
+    });
+    // req.session.userId = newUser._id;
+    res.render("login");
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(500).render("account-create", { errorMessage: error.message });
+    } else if (error.code === 11000) {
+      res.status(500).render("account-create", {
+        errorMessage: "This username already exists",
       });
-      return;
-    } else if (newUser.email != newUser.email2) {
-      res.render("account-create", {
-        errorMessage: "The emails you provided are not the same, please check again",
-      });
-      return;
-    } else if (newUser.password != newUser.password2) {
-      res.render("account-create", {
-        errorMessage: "The Passwords you provided are not the same, please check again",
-      });
-      return;
-    } else if (!passwordRegex.test(newUser.password)) {
-      res.render("account-create", {
-        errorMessage:
-          "The Passwords should contain at least 1 capital letter, 1 number and be minimum 7 characters long",
-      });
-      return;
-    } else if (!emailRegex.test(newUser.email)) {
-      res.render("account-create", {
-        errorMessage: "The Email you provided is invalid, please check again",
-      });
-      return;
+    } else {
+      next(error);
     }
-    try {
-      const pw = bcrypt.hashSync(newUser.password, salt);
-      newUser.password = pw;
-      await User.create({
-        name: newUser.name,
-        surname: newUser.surname,
-        username: newUser.username,
-        email: newUser.email,
-        password: newUser.password,
-      });
-     // req.session.userId = newUser._id;
-      res.render("login");
-    } catch (error) {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render("account-create", { errorMessage: error.message });
-      } else if (error.code === 11000) {
-        res.status(500).render("account-create", {
-          errorMessage: "This username already exists",
+  }
+});
+
+router.get("/login", (req, res) => {
+  console.log('SESSION LOGIN: ',req.session);
+  if (req.session.currentUser) {
+    const { username, password } = req.session.currentUser;
+    console.log('USERNAME: ', username, 'PASSWORD: ', password);
+    User.findOne({ username }).then((user) => {
+      if (password===user.password) {
+        res.render("user-profile", {
+          user,
+          userInSession: req.session.currentUser,
         });
       } else {
-        next(error);
+        res.render("login", { errorMessage: "Incorrect password." });
       }
-    }
-  });
+    });
+  } else {
+    res.render("login");
+  }
+});
 
-  router.get('/login', (req, res) => res.render('login'));
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
 
-  router.post('/login', (req, res, next) => {
-    const { username, password } = req.body;
+  console.log("SESSION =====> ", req.session);
 
-  
-      console.log('SESSION =====> ', req.session);
-     
-  
+  if (!username || !password) {
+    res.render("login", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
+    return;
+  }
 
-    if (!username || !password ) {
-      res.render('login', {
-        errorMessage: 'Please enter both, username and password to login.'
-      });
-      return;
-    }
-   
-    User.findOne({ username })
-      .then(user => {
-        if (!user) {
-          res.render('login', { errorMessage: 'Username is not registered. Try with other username.' });
-          return;
-        } else if (bcrypt.compareSync(password, user.password)) {
-          req.session.currentUser = user;
-          res.render("user-profile", {user, userInSession: req.session.currentUser });
-        } else {
-          res.render('login', { errorMessage: 'Incorrect password.' });
-        }
-      })
-      .catch(error => next(error));
-  });
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("login", {
+          errorMessage: "Username is not registered. Try with other username.",
+        });
+        return;
+      } else if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.render("user-profile", {
+          user,
+          userInSession: req.session.currentUser,
+        });
+      } else {
+        res.render("login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
 
 // router.get("/userProfile", (req, res, next) => {
 //   res.render("user-profile");
 // });
 
-  module.exports = router;
+module.exports = router;
